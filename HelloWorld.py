2ac3fff20e8d93dcaea0bc8cc45eb9b13e1b0af2
Lattana <lattana@hellosezame.com>
2023-03-07 12:18:01 +0100

    ex 1 / 2 / 3

---
+++ HelloWorld.py
@@ -0,0 +1,27 @@
+# Ex 1
+valeur = 6.893
+distance = 19.7
+
+vitesse = valeur * distance
+print("la vitesse est de " + str(round(valeur * distance, 1)) + " km/h")
+
+# Ex 2
+# print('Bonjour, quel est votre nom ?')
+# nom = str(input())
+# print('Bonjour ' + nom + ', quel âge avez-vous ?')
+# age = int(input())
+# print("Bonjour " + nom + ", vous avez " + str(age) + " ans.")
+
+# Ex 3
+
+# print('Veuillez choisir un "float" !')
+# float = float(input())
+# if float > 0 :
+#     print("La racine = " + str(float ** 0.5) + "")
+# elif float == 0 :
+#     print("La racine = " + str(float ** 0.5) + "")
+# else :
+#     print("Le nombre est négatif")
+
+# Ex 4
+
